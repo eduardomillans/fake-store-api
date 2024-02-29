7a9fb178533155555728eece8f13b08208825f10
Eduardo Millán <eduardomillanrs@gmail.com>
2024-02-29 14:58:05 -0600

    Update env variables

--- src/shared/env.ts
+++ src/shared/env.ts
@@ -3,6 +3,11 @@ import { z } from 'zod';
 const schema = z.object({
     NODE_ENV: z.enum(['development', 'production']),
     SERVER_PORT: z.string().transform(port => +port),
+    DB_HOST: z.string(),
+    DB_PORT: z.string().transform(port => +port),
+    DB_NAME: z.string(),
+    DB_USER: z.string(),
+    DB_PASS: z.string(),
 });
 
 export const env = schema.parse(process.env);
