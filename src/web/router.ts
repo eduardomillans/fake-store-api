08fb94ed9e897815c57ebd554975c3fcc0f83979
Eduardo Millán <eduardomillanrs@gmail.com>
2024-02-29 14:49:39 -0600

    Add express router
    Here we bind controller class method to specific routes

---
+++ src/web/router.ts
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import { type InjectionToken, container } from 'tsyringe';
+import { glob } from 'glob';
+import { METADATA_KEY } from '@/shared/constants';
+import type { ControllerMetadata } from '@/shared/types';
+
+export const router = Router();
+
+// Dynamic loading of controllers
+glob.sync(`${__dirname}/**/*.controller.*`).forEach(controller => require(controller));
+
+// Register the routes with their respective controllers
+const controllers: ControllerMetadata[] | undefined = Reflect.getOwnMetadata(METADATA_KEY.CONTROLLER, Reflect);
+
+if (!controllers) {
+    throw new Error(`No controllers have been found! Please ensure that you have register at least one Controller.`);
+}
+
+for (const { target, prefix, middlewares, routes } of controllers) {
+    const instance = container.resolve(target as InjectionToken);
+
+    for (const route of routes) {
+        middlewares.push(...route.middlewares);
+
+        router[route.method](`${prefix}${route.path}`, ...middlewares, instance[route.action].bind(instance));
+    }
+}
