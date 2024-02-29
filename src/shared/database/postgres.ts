7091671818d5fa650314d2b12149852667584231
Eduardo Millán <eduardomillanrs@gmail.com>
2024-02-29 14:58:15 -0600

    Add postgres connection and utility

---
+++ src/shared/database/postgres.ts
@@ -0,0 +1,10 @@
+import postgres from 'postgres';
+import { env } from '@/shared/env';
+
+export const sql = postgres({
+    host: env.DB_HOST,
+    port: env.DB_PORT,
+    database: env.DB_NAME,
+    user: env.DB_USER,
+    password: env.DB_PASS,
+});
