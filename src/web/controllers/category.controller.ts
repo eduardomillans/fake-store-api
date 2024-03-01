ee015a8c24b474096f8f1c0890375e03e33cc899
Eduardo Millán <eduardomillanrs@gmail.com>
2024-03-01 11:32:07 -0600

    Use validator middleware in category controller

--- src/web/controllers/category.controller.ts
+++ src/web/controllers/category.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from 'express';
 import { controller, httpPost } from '@/shared/decorators/http';
+import { validatorMiddleware } from '@/web/middlewares/validator.middleware';
 import { createCategorySchema } from '@/category/dtos/create-category.dto';
 import { CreateCategory } from '@/category/features/create-category';
 
@@ -7,11 +8,9 @@ import { CreateCategory } from '@/category/features/create-category';
 export default class CategoryController {
     public constructor(private readonly createCategory: CreateCategory) {}
 
-    @httpPost('/')
+    @httpPost('/', validatorMiddleware(createCategorySchema))
     public async create(req: Request, res: Response) {
-        const { name, imageUrl } = createCategorySchema.parse(req.body);
-
-        const category = await this.createCategory.handle({ name, imageUrl });
+        const category = await this.createCategory.handle(req.body);
 
         return res.json(category);
     }
