44a97b650f4565fcf0f089f7b9c1c8ee39e5bc76
Eduardo Millán <eduardomillanrs@gmail.com>
2024-03-01 11:32:22 -0600

    Handle server errors

--- src/web/server.ts
+++ src/web/server.ts
@@ -5,6 +5,7 @@ import morgan from 'morgan';
 import type { DependencyContainer } from 'tsyringe';
 import { env } from '@/shared/env';
 import { Router } from '@/web/router';
+import { ValidationException } from '@/shared/exceptions/validation.exception';
 
 export class Server {
     private app?: express.Application;
@@ -24,6 +25,19 @@ export class Server {
 
         // Routes
         this.app.use('/api', Router.routes(this.container));
+
+        // Error
+        this.app.use((error: express.ErrorRequestHandler, _: express.Request, res: express.Response, next: express.NextFunction) => {
+            if (error instanceof ValidationException) {
+                return res.status(400).json({ message: error.message });
+            }
+
+            if (error instanceof Error) {
+                return res.status(500).json({ message: error.message });
+            }
+
+            next();
+        });
     }
 
     public async start(): Promise<void> {
